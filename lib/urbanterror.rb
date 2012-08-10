3c19c4980841caf728772bc283f998dce8d6c0b8
Ricky Elrod <ricky@elrod.me>
2012-08-09 20:15:36 -0400

    release 3.0.0.
    - Follow ruby conventions better.
    - Update for Urban Terror 4.2.

--- lib/urbanterror.rb
+++ lib/urbanterror.rb
@@ -6,27 +6,23 @@ require 'pp'
 class UrbanTerror
   def initialize(server, port=nil, rcon=nil)
     @server = server
-    @port = port.nil? ? 27960 : port
-    @rcon = rcon.nil? ? '' : rcon
+    @port = port || 27960
+    @rcon = rcon || ''
     @socket = UDPSocket.open
   end
 
-  def sendCommand(command)
+  def send_command(command)
     magic = "\377\377\377\377"
     @socket.send("#{magic}#{command}\n", 0, @server, @port)
     @socket.recv(2048)
   end
 
   def get(command)
-    sendCommand("get#{command}")
+    send_command("get#{command}")
   end
 
-  def getparts(command, i)
-    get(command).split("\n")[i]
-  end
-  
   def rcon(command)
-    sendCommand("rcon #{@rcon} #{command}")
+    send_command("rcon #{@rcon} #{command}")
   end
 
   # settings() returns a hash of settings => values.
@@ -34,14 +30,14 @@ class UrbanTerror
   # doing the same thing and just selecting one from the Hash, so
   # why not just let the user do server.settings['map'] or whatever.
   def settings
-    result = getparts("status", 1).split("\\").reject(&:empty?)
+    result = get_parts("status", 1).split("\\").reject(&:empty?)
     Hash[*result]
   end
   
   # players() returns a list of hashes. Each hash contains
   # name, score, ping.
   def players
-    results = getparts("status", 2..-1)
+    results = get_parts("status", 2..-1)
     results.map do |player|
       player = player.split(" ", 3)
       {
@@ -64,18 +60,19 @@ class UrbanTerror
 
   MAX_GEAR = 63
   
-  def self.gearCalc(gearArray)
-    gearArray.each{ |w| raise "No such gear type '#{w}'" unless GEAR_TYPES.has_key?(w) }
-    MAX_GEAR - gearArray.map{|w| GEAR_TYPES[w] }.reduce(:+)
+  def self.gear_calc(gear_array)
+    gear_array.each{ |w| raise "No such gear type '#{w}'" unless GEAR_TYPES.has_key?(w) }
+    MAX_GEAR - gear_array.map{|w| GEAR_TYPES[w] }.reduce(:+)
   end
   
-  def self.reverseGearCalc(number)
+  def self.reverse_gear_calc(number)
     raise "#{number} is outside of the range 0 to 63." unless (0..63).include?(number)
     GEAR_TYPES.select{|weapon, gear_val| number & gear_val == 0 }.map(&:first)
   end
 
   GAME_MODES = {
     0 => ['Free For All',      'FFA'],
+    1 => ['Last Man Standing', 'LMS'],
     3 => ['Team Death Match',  'TDM'],
     4 => ['Team Survivor',     'TS'],
     5 => ['Follow the Leader', 'FTL'],
@@ -84,8 +81,13 @@ class UrbanTerror
     8 => ['Bomb mode',         'BOMB']
   }
   
-  def self.matchType(number, abbreviate=false)
+  def self.match_type(number, abbreviate=false)
     raise "#{number} is not a valid gametype." unless GAME_MODES.has_key? number
     GAME_MODES[number][abbreviate ? 1 : 0]
   end  
+
+  private
+  def get_parts(command, i)
+    get(command).split("\n")[i]
+  end
 end
--- urbanterror.gemspec
+++ urbanterror.gemspec
@@ -1,9 +1,10 @@
 Gem::Specification.new do |s|
   s.name = 'urbanterror'
-  s.version = '1.0'
-  s.date = '2010-08-29'
+  s.version = '3.0.0'
+  s.date = '2012-08-09'
   s.authors = ['Ricky Elrod']
   s.email = 'ricky@elrod.me'
+  s.license = 'MIT'
   s.summary = 'Provides a class to access and control Urban Terror servers via RCON over UDP.'
   s.description = 'Provides a class to access and control Urban Terror servers via RCON over UDP.'
   s.files = ['lib/urbanterror.rb']
